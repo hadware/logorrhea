39610f3da906b021b16f42e0769706407d72045d
hadware <hadwarez@gmail.com>
2017-06-18 17:55:23 +0200

    Init with files from the pytorch repo

---
+++ helpers.py
@@ -0,0 +1,35 @@
+# https://github.com/spro/practical-pytorch
+
+import unidecode
+import string
+import random
+import time
+import math
+import torch
+from torch.autograd import Variable
+
+# Reading and un-unicode-encoding data
+
+all_characters = string.printable
+n_characters = len(all_characters)
+
+def read_file(filename):
+    file = unidecode.unidecode(open(filename).read())
+    return file, len(file)
+
+# Turning a string into a tensor
+
+def char_tensor(string):
+    tensor = torch.zeros(len(string)).long()
+    for i, char in enumerate(string):
+        tensor[i] = all_characters.index(char)
+    return Variable(tensor)
+
+# Readable time elapsed
+
+def time_since(since):
+    s = time.time() - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
