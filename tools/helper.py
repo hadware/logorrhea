9f68ffaae6ad541fc770bd62331163472f2a8916
Hadrien Titeux <hadwarez@gmail.com>
2017-07-07 14:38:51 +0200

    Commence à construire le gestionnaire de fichier

--- tools/helper.py
+++ tools/helper.py
@@ -1,13 +1,41 @@
-import voxpopuli
+from voxpopuli import PhonemeList, Voice
+from voxpopuli.phonemes import Phoneme
 import torch
+import logging
+from progressbar import ProgressBar
+import random
+from typing import List
+
 
 class CorpusManager:
 
     def __init__(self, filepath : str, lang="fr"):
+        self.voice = Voice(lang=lang)
         self.alphabet_size = None
+        phonemized_file = [] # type: List[Phoneme]
+        with open(filepath) as corpus_file:
+            for line in corpus_file:
+                phonemized_file += self.voice.to_phonemes(line)
+        self._build_phonemes_stats(phonemized_file)
 
-    def get_random_chunk(self, chunk_size):
+        self.phonemes_list = [phoneme.name for phoneme in phonemized_file]
+
+    def _build_phonemes_stats(self, phonemized_file):
+        logging.info("Building phonemes stats for future synthesizing")
+        bar = ProgressBar()
+        for phoneme in bar(phonemized_file):
+            pass
+
+    def _to_tensor(self, phoneme_list : List[str]):
         pass
+
+    def random_training_set(self, chunk_size):
+        start_index = random.randint(0, len(self.phonemes_list) - chunk_size)
+        end_index = start_index + chunk_len + 1
+        chunk = self.phonemes_list[start_index:end_index]
+        inp = self._to_tensor(chunk[:-1])
+        target = self._to_tensor(chunk[1:])
+        return inp, target
 
     def synthesize(self, phonemes_list : List[str]):
         """converts a list of phonemes (only their names) to a wav file"""
--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import argparse
 
 import time
 import torch
+from torch import nn
+from tools.model import RNN
+from tools.helper import CorpusManager
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('filename', type=str)
@@ -12,10 +15,26 @@ argparser.add_argument('--n_layers', type=int, default=2)
 argparser.add_argument('--learning_rate', type=float, default=0.01)
 argparser.add_argument('--chunk_len', type=int, default=200)
 
+def train(inp, target):
+    hidden = decoder.init_hidden()
+    decoder.zero_grad()
+    loss = 0
+
+    for c in range(args.chunk_len):
+        output, hidden = decoder(inp[c], hidden)
+        loss += criterion(output, target[c])
+
+    loss.backward()
+    decoder_optimizer.step()
+
+    return loss.data[0] / args.chunk_len
+
+
 if __name__ == "__main__":
     args = argparser.parse_args()
+    corpus = CorpusManager(args.filename)
 
-    decoder = RNN(n_characters, args.hidden_size, n_characters, args.n_layers)
+    decoder = RNN(corpus.alphabet_size, args.hidden_size, corpus.alphabet_size, args.n_layers)
     decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=args.learning_rate)
     criterion = nn.CrossEntropyLoss()
 
